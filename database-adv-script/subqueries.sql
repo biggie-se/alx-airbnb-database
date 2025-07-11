d2488f01a9dd3d742e99d34a31ed6249487e0d7e
Anthony Sogomo <sogomo.anthony1@gmail.com>
2025-07-11 16:03:28 +0300

    Added comments to subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,3 +1,5 @@
+--- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
+
 SELECT p.*, (
     SELECT AVG(r.rating)
     FROM Review r
@@ -10,6 +12,8 @@ WHERE (
     WHERE r.property_id = p.property_id
 ) > 4.0;
 
+--- Write a correlated subquery to find users who have made more than 3 bookings.
+
 SELECT u.*, (
     SELECT COUNT(b.status)
     FROM Booking b
@@ -20,5 +24,5 @@ WHERE (
     SELECT COUNT(b.status)
     FROM Booking b
     WHERE b.user_id = u.user_id AND b.status = 'confirmed' 
-) > 3;
-ORDER BY confirmed_bookings DESC
+) > 3
+ORDER BY confirmed_bookings DESC;
